e41f9dd15aca24c72ffa0149a1e8904b2442dcab
Grant Eben <gmeben@gmail.com>
2024-04-22 16:22:09 -0400

    feat: Register search page block

--- src/blocks/blocks.php
+++ src/blocks/blocks.php
@@ -11,3 +11,10 @@ if ( CCC_FEATURE_PROFILE_BLOCK ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'profile/server.php' );
 }
 
+function register_search_page_block() {
+	register_block_type(
+		CC_CLIENT_BASE_DIR . '/build/search-page',
+		[]
+	);
+}
+add_action( 'init', __NAMESPACE__ . '\register_search_page_block' );
