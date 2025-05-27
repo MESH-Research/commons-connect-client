c77d339790e9c08ca23e268b5e490f5a4b99d93c
Mike Thicke <mikethicke@gmail.com>
2025-05-27 10:12:42 -0400

    Remove profiles block, dependency update

--- cc-client.php
+++ cc-client.php
@@ -15,66 +15,59 @@
 
 namespace MeshResearch\CCClient;
 
-if ( ! defined( 'CC_CLIENT_DOING_TESTING') ) {
-	define( 'CC_CLIENT_DOING_TESTING', false );
+if (!defined("CC_CLIENT_DOING_TESTING")) {
+    define("CC_CLIENT_DOING_TESTING", false);
 }
 
-define( 'CC_CLIENT_BASE_DIR', plugin_dir_path( __FILE__ ) );
-define( 'CC_CLIENT_BASE_URL', plugin_dir_url( __FILE__ ) );
+define("CC_CLIENT_BASE_DIR", plugin_dir_path(__FILE__));
+define("CC_CLIENT_BASE_URL", plugin_dir_url(__FILE__));
 
-define('CC_CLIENT_REST_NAMESPACE', 'cc-client/v1');
-
-/**
- * Feature Flags
- */
-
-define( 'CCC_FEATURE_PROFILE_BLOCK', false );
+define("CC_CLIENT_REST_NAMESPACE", "cc-client/v1");
 
 /**
  * Enqueue block editor frontend assets.
  */
-function enqueue_client_block_assets() {
-    $asset_file = include CC_CLIENT_BASE_DIR . 'build/profile/front.asset.php';
+function enqueue_client_block_assets()
+{
+    $asset_file = include CC_CLIENT_BASE_DIR . "build/search/view.asset.php";
     wp_enqueue_style(
-        'cc-client-profile',
-        CC_CLIENT_BASE_URL . 'build/profile/style-index.css',
+        "cc-client-search",
+        CC_CLIENT_BASE_URL . "build/search/style-view.css",
         [],
-        $asset_file['version']
+        $asset_file["version"]
     );
 
-    $asset_file = include CC_CLIENT_BASE_DIR . 'build/search/view.asset.php';
-    wp_enqueue_style(
-        'cc-client-search',
-        CC_CLIENT_BASE_URL . 'build/search/style-view.css',
-        [],
-        $asset_file['version']
-    );
-
-    if ( is_admin() ) {
-
+    if (is_admin()) {
         // Enqueue editor only assets here
     }
 }
-add_action( 'enqueue_block_assets', __NAMESPACE__ . '\enqueue_client_block_assets' );
+add_action(
+    "enqueue_block_assets",
+    __NAMESPACE__ . '\enqueue_client_block_assets'
+);
 
 /**
  * Composer autoload.
- * 
+ *
  * When cc-client is installed as a conmposer dependency, the vendor directory
  * will be in the root of the project and the autoload.php will already be
  * included at the project level, so the vendor directory will not exist.
  */
-if ( file_exists( CC_CLIENT_BASE_DIR . 'vendor/autoload.php' ) ) {
-    require_once( CC_CLIENT_BASE_DIR . 'vendor/autoload.php' );
+if (file_exists(CC_CLIENT_BASE_DIR . "vendor/autoload.php")) {
+    require_once CC_CLIENT_BASE_DIR . "vendor/autoload.php";
 }
 
-require_once( CC_CLIENT_BASE_DIR . 'src/admin/admin-settings.php' );
-require_once( CC_CLIENT_BASE_DIR . 'src/Rest/rest.php' );
-require_once( CC_CLIENT_BASE_DIR . 'src/blocks/blocks.php' );
-require_once( CC_CLIENT_BASE_DIR . 'src/functions.php' );
+require_once CC_CLIENT_BASE_DIR . "src/admin/admin-settings.php";
+require_once CC_CLIENT_BASE_DIR . "src/Rest/rest.php";
+require_once CC_CLIENT_BASE_DIR . "src/blocks/blocks.php";
+require_once CC_CLIENT_BASE_DIR . "src/functions.php";
 
-require_once( CC_CLIENT_BASE_DIR . 'src/Search/Provisioning/bulk_provisioning.php' );
-require_once( CC_CLIENT_BASE_DIR . 'src/Search/Provisioning/incremental_provisioner_functions.php' );
-require_once( CC_CLIENT_BASE_DIR . 'src/Search/Provisioning/provisionable_functions.php' );
-require_once( CC_CLIENT_BASE_DIR . 'src/Search/Provisioning/provisioning_helper_functions.php' );
-require_once( CC_CLIENT_BASE_DIR . 'src/Search/search_functions.php' );
+require_once CC_CLIENT_BASE_DIR .
+    "src/Search/Provisioning/bulk_provisioning.php";
+require_once CC_CLIENT_BASE_DIR .
+    "src/Search/Provisioning/incremental_provisioner_functions.php";
+require_once CC_CLIENT_BASE_DIR .
+    "src/Search/Provisioning/provisionable_functions.php";
+require_once CC_CLIENT_BASE_DIR .
+    "src/Search/Provisioning/provisioning_helper_functions.php";
+require_once CC_CLIENT_BASE_DIR . "src/Search/search_functions.php";
--- src/blocks/blocks.php
+++ src/blocks/blocks.php
@@ -7,11 +7,8 @@
 
 namespace MeshResearch\CCClient;
 
-if ( CCC_FEATURE_PROFILE_BLOCK ) {
-    require_once( plugin_dir_path( __FILE__ ) . 'profile/server.php' );
+function register_search_block()
+{
+    register_block_type(CC_CLIENT_BASE_DIR . "/build/search");
 }
-
-function register_search_block() {
-    register_block_type(CC_CLIENT_BASE_DIR . '/build/search');
-}
-add_action( 'init', __NAMESPACE__ . '\register_search_block' );
+add_action("init", __NAMESPACE__ . '\register_search_block');
